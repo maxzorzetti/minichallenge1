488d0307665a3af1472bab08f49b2ff6d9a1bb74
Laura Corssac <lauracorssac@hotmail.com>
2017-04-27 10:52:32 -0300

    added news feed class

---
+++ SportsApp/SANewsFeedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SANewsFeedTableViewController.h
+//  SportsApp
+//
+//  Created by Laura Corssac on 27/04/17.
+//  Copyright © 2017 Bruno Scheltzke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SANewsFeedTableViewController : UITableViewController
+
+@end
