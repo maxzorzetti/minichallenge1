3e63f3e7ee160148678bfa1c001510cd11d69c1e
Laura Corssac <lauracorssac@hotmail.com>
2017-04-27 11:18:56 -0300

    added news feed cell

---
+++ SportsApp/SANewsFeedTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SANewsFeedTableViewCell.h
+//  SportsApp
+//
+//  Created by Laura Corssac on 27/04/17.
+//  Copyright © 2017 Bruno Scheltzke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SANewsFeedTableViewCell : UITableViewCell
+
+@end
---
+++ SportsApp/SANewsFeedTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  SANewsFeedTableViewCell.m
+//  SportsApp
+//
+//  Created by Laura Corssac on 27/04/17.
+//  Copyright © 2017 Bruno Scheltzke. All rights reserved.
+//
+
+#import "SANewsFeedTableViewCell.h"
+
+@implementation SANewsFeedTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
